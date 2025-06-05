b1e5f335d95b88541bea182226dd7b9667a3402e
lannory <detochkanikolay@gmail.com>
2025-06-05 20:36:02 +0300

    some fixes

--- src/AddForm.jsx
+++ src/AddForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch} from 'react-redux';
 import { increase } from "./store/counterSlice";
 import { createNewTask } from "./store/todoSlice";
@@ -8,20 +8,16 @@ function AddForm() {
 
 	const dispatch = useDispatch()
 
-	let taskValue;
+	const taskRef = useRef();
 
 	const handleSubmit = () =>{
-		dispatch(createNewTask({value: taskValue, id: Date().now}));
+		dispatch(createNewTask({value: taskRef.current.value, id: Date().now}));
 		dispatch(increase());
-	}
-
-	const handleChange = (value) =>{
-		taskValue = value;
 	}
 
 	return (
 		<form>
-			<input type="text" placeholder='new task' onChange={e => handleChange(e.target.value)}/>
+			<input type="text" placeholder='new task' ref={taskRef}/>
 			<button type='button' onClick={handleSubmit}>sumbit</button>
 		</form>
 	);
